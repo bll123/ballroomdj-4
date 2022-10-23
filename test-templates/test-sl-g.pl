8715332b529de4c18555a6143c5f49ae0bf8c2a5
Brad Lanam <brad.lanam.di@gmail.com>
2022-10-23 09:48:03 -0700

    testsuite: stop-at

---
+++ test-templates/test-sl-g.pl
@@ -0,0 +1,28 @@
+# playlist
+# 2022-10-21
+#
+# gap set to 0 seconds
+# stop-after set to 2
+#
+version
+..1
+ALLOWEDKEYWORDS
+..
+DANCERATING
+..Poor
+GAP
+..0
+MAXPLAYTIME
+..
+PLAYANNOUNCE
+..no
+STOPAFTER
+..2
+STOPTIME
+..0
+DANCELEVELLOW
+..Easy
+DANCELEVELHIGH
+..Advanced
+TYPE
+..songlist
