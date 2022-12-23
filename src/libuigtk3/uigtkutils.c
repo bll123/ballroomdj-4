d3635678ce5de17f0a2b7088b78251f9b6a32c72
Brad Lanam <brad.lanam.di@gmail.com>
2022-12-23 11:42:00 -0800

    ui: fix text entry color.

--- src/libuigtk3/uigtkutils.c
+++ src/libuigtk3/uigtkutils.c
@@ -109,6 +109,8 @@ uiSetUIFont (char *uifont)
 
     tcss = gtk_css_provider_new ();
     snprintf (tbuff, sizeof (tbuff), "* { font-family: '%s'; } ", wbuff);
+    snprintf (wbuff, sizeof (wbuff), "entry { color: @theme_fg_color; } ");
+    strlcat (tbuff, wbuff, sizeof (tbuff));
     if (sz > 0) {
       snprintf (wbuff, sizeof (wbuff), " * { font-size: %dpt; } ", sz);
       strlcat (tbuff, wbuff, sizeof (tbuff));
