57ccff3bdb8edf5da142bb868205c350f06c8794
Brad Lanam <brad.lanam.di@gmail.com>
2023-09-21 11:46:21 -0700

    cleanup: macos

--- src/utils/macfixrpath.sh
+++ src/utils/macfixrpath.sh
@@ -15,22 +15,16 @@ fi
 #LPATH=@executable_path
 #LPATH=@loader_path
 # the rpath has now been updated by cmake files to point
-# to the correct paths (it appears to use @loader_path).
+# to the correct paths (using @loader_path).
 LPATH=@rpath
 
 starget=$(basename $target)
 liblist=""
 
-# someday
-# would probably be good to try and fix libvorbsifile and libicu to
-# have the proper link paths in the pkgconfig files.
-
 # /Volumes/Users/bll/bdj4/packages/icu/lib/libicudata.72.dylib
-# /Volumes/Users/bll/bdj4/bin/../plocal/lib/libvorbisfile.3.dylib
 count=0
 cmd="install_name_tool "
-for l in libicudata libicui18n libicuuc \
-    libvorbisfile.3 libvorbis.0; do
+for l in libicudata libicui18n libicuuc; do
   path=$(otool -L $target | grep $l | sed 's,^[^/]*,,;s,dylib .*,dylib,')
   if [[ $path != "" ]]; then
     liblist+=$l
