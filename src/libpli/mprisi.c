ddce05722f1f447cbc2d582b870eadef49ab04ee
Brad Lanam <brad.lanam.di@gmail.com>
2023-12-20 06:28:58 -0800

    cleanup

---
+++ src/libpli/mprisi.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2023 Brad Lanam Pleasant Hill CA
+ */
+#include "config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include "dbus.h"
+#include "mdebug.h"
+
