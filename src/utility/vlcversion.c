93b3932ccfd30fe6bcd24479fc9291fbcc3d5915
Brad Lanam <brad.lanam.di@gmail.com>
2024-07-18 11:38:39 -0700

    4.11.0

---
+++ src/utility/vlcversion.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2024 Brad Lanam Pleasant Hill CA
+ */
+#include "config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#if _lib_libvlc3_new || _lib_libvlc4_new
+# include <vlc/vlc.h>
+# include <vlc/libvlc_version.h>
+#endif
+
+int
+main (int argc, char *argv [])
+{
+#if _lib_libvlc3_new || _lib_libvlc4_new
+  fprintf (stderr, "%s\n", libvlc_get_version());
+#else
+  fprintf (stderr, "NG\n");
+#endif /* libvlc_new() */
+}
