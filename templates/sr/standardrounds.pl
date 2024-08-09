ab439841ffdaddfd528ac60c477579f0d8ca6b0d
Brad Lanam <brad.lanam.di@gmail.com>
2024-08-09 13:57:36 -0700

    translations: add serbian

---
+++ templates/sr/standardrounds.pl
@@ -0,0 +1,29 @@
+# playlist
+# 2024-2-16
+# version 3
+version
+..1
+ALLOWEDKEYWORDS
+..
+DANCERATING
+..Добро
+GAP
+..3000
+MAXPLAYTIME
+..0
+PLAYANNOUNCE
+..no
+STOPAFTER
+..0
+STOPTIME
+..0
+DANCELEVELLOW
+..Лако
+DANCELEVELHIGH
+..Напредно
+TAGS
+..
+TAG_WEIGHT
+..
+TYPE
+..sequence
