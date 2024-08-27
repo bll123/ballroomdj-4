5ff119a88c24efe1d2979ff25a4ca0fed0594300
Brad Lanam <brad.lanam.di@gmail.com>
2024-08-27 09:22:25 -0700

    4.11.6

--- src/utils/bdj4config.cmake
+++ src/utils/bdj4config.cmake
@@ -382,7 +382,7 @@ if (NOT WIN32)
     # 10.14 = Mojave, 10.15 = Catalina
     # 11 = Big Sur, 12 = Monterey, 13 = Ventura, 14 = Sonoma
     # 15 = Sequoia
-    # update this in pkg/macos/Info.plist also
+    # IMPORTANT: update this in pkg/macos/Info.plist also
     set (CMAKE_OSX_DEPLOYMENT_TARGET 11)
   endif()
 
